b32671e0edc601b10a31174bcc6aad080d452ef0
chinky23-tech <chinky0330@gmail.com>
2025-09-13 19:29:13 +0530

    conter

---
+++ script.js
@@ -0,0 +1,19 @@
+
+let timer = document.getElementById('timer');
+let start = 10;
+
+//countdown using for loop
+
+for( let i = start; i >= 0; i --){
+
+    setTimeout(() =>{
+if(i === 0){
+    timer.textContent = "Time's Up!";
+}else{
+    timer.textContent = i;
+}
+    
+    }, (start - i) * 1000);
+}
+
+
